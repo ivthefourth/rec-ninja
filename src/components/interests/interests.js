33ae9d28f2e90f7a715b9cc76cc778430ea3d88c
Howard Shaw <chip.shaw4@gmail.com>
2017-09-25 00:49:36 -0600

    moved interests code to correct component

--- src/components/interests/interests.js
+++ src/components/interests/interests.js
@@ -1,3 +1,40 @@
 import './interests.css';
 import state from '../state/state';
 
+    
+   
+ function addChip() {
+   for (let i = 0; i < state.interests.all.length; i++) {
+      
+      let newChip = $('<div class="chip center"></div>');
+      $("#unselected-interests").append(newChip.text(state.interests.all[i].name));
+      
+      $(newChip).click(function() {
+         state.interests.all[i].toggle();
+      });
+   state.interests.all[i].on('change', function(e) {
+      
+      if(e.val) {
+         newChip.addClass("selected");
+         $("#selected-interests").append(newChip);
+      } else {
+         newChip.removeClass('selected');
+         $("#unselected-interests").prepend(newChip);
+      }
+
+   });
+   }
+}
+
+$(document).ready(function(){
+   addChip();
+
+
+   $("#clear-interests").click(function() {
+   
+      state.interests.selected.forEach(function(clear) {
+         clear.update(false, true);
+      });
+      state.interests.emit('change');
+   });
+})
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,54 +3,6 @@ import state from '../state/state';
 
 $(document).ready(function() {
     $('select').material_select();
-    
-	
-    function addChip() {
-		for (let i = 0; i < state.interests.all.length; i++) {
-			
-			let newChip = $('<div class="chip center"></div>');
-			$("#unselected-interests").append(newChip.text(state.interests.all[i].name));
-			
-			$(newChip).click(function() {
-				state.interests.all[i].toggle();
-			});
-// =========================
-			// if (localStorage.getItem('interests') !== null) {
-			// 	state.interests.emit('change');
-			
-			// if (localStorage.getItem('interests') !== null) {
-			// 	let interestsArray = JSON.parse(localStorage.getItem('interests'));
-				
-
-			// 	if (interestsArray[state.interests.all[i].id] === true ) {
-			// 		state.interests.all[i].selected = true;
-			// 	}
-			// }
-// ==========================
-		state.interests.all[i].on('change', function(e) {
-			
-			if(e.val) {
-				newChip.addClass("selected");
-				$("#selected-interests").append(newChip);
-			} else {
-			 	newChip.removeClass('selected');
-			 	$("#unselected-interests").prepend(newChip);
-			}
-
-		});
-		}
-	}
-
-	addChip();
-
-
-	$("#clear-interests").click(function() {
-	
-		state.interests.selected.forEach(function(clear) {
-			clear.update(false, true);
-		});
-		state.interests.emit('change');
-	});
 	
 	$(".destination-input").on('focus', function() {
  		if ($("#interests-header").hasClass('active')) {
